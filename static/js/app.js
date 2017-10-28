73c8ec3893c587944b0f7ab6ce6baeef41345018
adityathebe <thebeaditya@gmail.com>
2017-10-28 10:23:15 +0545

    changed refresh interval

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 const CRYPTOCOMPARE_API_URI = "https://min-api.cryptocompare.com";
 const COINMARKETCAP_API_URI = "https://api.coinmarketcap.com/";
 const BASE_IMAGE_URL = "https://www.cryptocompare.com"
-const UPDATE_INTERVAL = 600 * 1000;
+const UPDATE_INTERVAL = 60 * 1000;
 
 let app = new Vue({
     el: '#app',
